9a69f944ce9b41e99a97c5a7c5e0db61152ff4a5
RomanD1995 <eloyroman101@gmail.com>
2026-07-02 21:24:08 -0600

    fin de clase 7

--- semana_7/clase07_analisis_emprendimientos.py
+++
@@ -1,8 +0,0 @@
-"""Practica Semana 07: analisis de emprendimientos costarricenses.
-
-Complete los espacios marcados con TODO. El objetivo es generar un reporte por
-sede usando listas, diccionarios, funciones, ciclos y condicionales.
-"""
-
-from sedes import sedes
-
